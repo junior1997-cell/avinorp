da0749396fc7699fd00202ddcf96ba74dde4aca6
davidrequejo <requejodavidmelvin@gmail.com>
2025-07-16 18:52:24 -0500

    cambio 1607202518

--- config/global_local.php
+++ config/global_local.php
@@ -3,7 +3,7 @@
 define("DB_HOST","localhost");
 
 //Nombre de la base de datos
-define("DB_NAME", "libreria_sistema");
+define("DB_NAME", "avinorp");
 
 //Usuario de la base de datos
 define("DB_USERNAME", "root");
